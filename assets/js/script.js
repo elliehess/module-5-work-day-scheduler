8ea8eb05f0a47ad650ba063c4e09815df4dad50f
Ellie <ellie@Ellies-MBP.attlocal.net>
2023-01-03 20:46:44 -0800

    added timetracker function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,61 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+  
+//display today's date and time 
+  var currentDayEl = $('#currentDay');
 
-var currentDayEl = $('#currentDay');
+  function displayTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    currentDayEl.text(rightNow);
+  };
+
 
 $(document).ready(function () {
     // Add a listener for click events on the save button. 
-    $(".saveBtn").on("click"), function () {
+    $(".saveBtn").on("click", function () {
       //use jquery to get values 
       var textinputEl = $(this).siblings(".description").val();
       var timeinputEl = $(this).parent().attr("id");
 
       //save these values to local storage 
       localStorage.setItem(timeinputEl, textinputEl);
+    });
+
+    function timeTracker (){
+              //get the current time and number of hours
+              var timeNow = moment().hour();
+
+              // create a loop for the time blocks 
+              $(".time-block").each(function () {
+                  var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+      
+                  // create an if statement to check the time and add appropriate background classes
+                  if (blockTime < timeNow) {
+                      $(this).removeClass("future");
+                      $(this).removeClass("present");
+                      $(this).addClass("past");
+                  }
+                  else if (blockTime === timeNow) {
+                      $(this).removeClass("past");
+                      $(this).removeClass("future");
+                      $(this).addClass("present");
+                  }
+                  else {
+                      $(this).removeClass("present");
+                      $(this).removeClass("past");
+                      $(this).addClass("future");
+      
+                  }
+              })
+          }
     }
 
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // 
-  
+    //Get the items from local storage if any stored
+    $("#hour-9 .description").val(localStorage.getItem)("hour-9"));
 
-  });
+    timeTracker();
 
-  //display today's date and time 
-  function displayTime() {
-    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-    currentDayEl.text(rightNow);
-  }
 
   displayTime(); //run function for today's date
   setInterval(displayTime, 1000); //automatically update 
